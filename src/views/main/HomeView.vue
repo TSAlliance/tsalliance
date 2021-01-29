736211dc493906fc6f0834b3da31fd5d4a0f12d4
Cedric Zitzmann <cedric.zitzmann@gmail.com>
2021-01-29 17:40:31 +0100

    Added footer

--- src/views/main/HomeView.vue
+++ src/views/main/HomeView.vue
@@ -2,6 +2,7 @@
     <div class="section-container">
         <app-header></app-header>
         <section-landing></section-landing>
+        <app-footer></app-footer>
     </div>
 </template>
 
@@ -9,6 +10,7 @@
 import { defineComponent } from 'vue';
 
 import AppHeader from '@/views/shared/AppHeaderView.vue'
+import AppFooter from '@/views/shared/AppFooterView.vue'
 import SectionLanding from '@/views/main/sections/AppLandingSection.vue'
 
 import services from '@/config/services.ts'
@@ -16,6 +18,7 @@ import services from '@/config/services.ts'
 export default defineComponent({
     components: {
         AppHeader,
+        AppFooter,
         SectionLanding
     },
     data() {
